654d000be4d043dfaeb678df6a9ef7eb406a7796
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-08-08 12:25:26 +0800

    card for account

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -2,8 +2,10 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class AddStoryView(generic.CreateView):
+class AddStoryView(LoginRequiredMixin, generic.CreateView):
+    login_url = '/users/login/'
     form_class = StoryForm
     context_object_name = 'storyForm'
     template_name = 'news/createStory.html'
